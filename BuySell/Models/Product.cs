1ecf16da070fb6ed2326f80a8191910967533088
Javid Atakishiyev <javidatakishiyev@hotmail.com>
2020-02-24 18:08:19 +0400

    1 qabagha 2 dala

--- BuySell/Models/Product.cs
+++ BuySell/Models/Product.cs
@@ -16,25 +16,36 @@ namespace BuySell.Models
         public int CategoryId { get; set; }
         [Required]
         [MaxLength(50)]
+        [DisplayName("Product Name")]
         public string ProductName { get; set; }
         [Required]
         [Column(TypeName = "decimal(18,2)")]
+        [DisplayName("Product Price")]
         public decimal ProductPrice { get; set; }
         [Required]
+        [DisplayName("Product Discount")]
         public int ProductDiscount { get; set; }
         [Required]
         [DefaultValue(0)]
+        [DisplayName("Product Rating")]
         public byte ProductRating { get; set; }
         [Required]
         [MaxLength(500)]
+        [DisplayName("Product Details")]
         public string ProductDetails { get; set; }
         [Required]
+        [DisplayName("Product Count")]
         public int ProductCount { get; set; }
         [Required]
+        [DisplayName("Product Availablity Status")]
         public bool IsAvailable { get; set; }
+        [DisplayName("Product Remove Status")]
         public bool IsDeleted { get; set; }
+        [DisplayName("Additon Date")]
         public DateTime AddedDate { get; set; }
+        [DisplayName("Modification Date")]
         public DateTime? ModifiedDate { get; set; }
+        [DisplayName("Deletion Date")]
         public DateTime? DeletedDate { get; set; }
         public ICollection<Photo> Photos { get; set; }
         public ICollection<Comment> Comments { get; set; }
